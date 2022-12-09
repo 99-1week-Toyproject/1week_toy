4ba31f82fce3d4e851fb488e97092e17ecdc95c3
hyeunseungYu <hyeunseung03@gmail.com>
2022-12-09 19:56:32 +0900

    [#1] [update]깃허브 연동 확인
    깃허브 연동 확인 테스트 커밋입니다.

--- app.py
+++ app.py
@@ -1,9 +1,54 @@
+from pymongo import MongoClient
 import requests
 from bs4 import BeautifulSoup
+from flask import Flask, render_template, request, jsonify
 
-headers = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
-data = requests.get('https://movie.naver.com/movie/sdb/rank/rmovie.naver?sel=pnt&date=20210829',headers=headers)
+app = Flask(__name__)
+
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
+data = requests.get(
+    'https://movie.naver.com/movie/sdb/rank/rmovie.naver?sel=pnt&date=20210829', headers=headers)
 
 soup = BeautifulSoup(data.text, 'html.parser')
 
-# 코딩 시작
+client = MongoClient(
+    'mongodb+srv://hyeunseung:qlqjsgustmd1)@cluster0.jwlfweq.mongodb.net/?retryWrites=true&w=majority')
+db = client.Practice
+
+
+@app.route('/')
+def home():
+    return render_template('board.html')
+
+
+@app.route("/board/get", methods=["GET"])
+def board_get():
+    board_list = list(db.gameReview.find({}, {'_id': False}))
+
+    return jsonify({'board_list': board_list})
+
+
+@app.route("/board/post", methods=["POST"])
+def board_post():
+    board_title_receive = request.form['board_title_give']
+    board_id_receive = request.form['board_id_give']
+    board_text_receive = request.form['board_text_give']
+
+    board_list = list(db.gameReview.find({}, {'_id': False}))
+    board_cnt = len(board_list) + 1
+
+    insertDoc = {
+        'index': board_cnt,
+        'title': board_title_receive,
+        'id': board_id_receive,
+        'text': board_text_receive
+    }
+
+    db.gameReview.insert_one(insertDoc)
+
+    return jsonify({'msg': "등록완료"})
+
+
+if __name__ == '__main__':
+    app.run('0.0.0.0', port=5000, debug=True)

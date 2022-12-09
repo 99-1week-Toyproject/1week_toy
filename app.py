7a6537a69f7981332b1679701781a1875b824c5d
hyeunseungYu <hyeunseung03@gmail.com>
2022-12-10 02:50:38 +0900

    [#1] [update] 토픽 게시 및 댓글 기능 구현
    현재 토픽 게시 및 댓글 기능 구현 완료하였습니다!

--- app.py
+++ app.py
@@ -50,5 +50,29 @@ def board_post():
     return jsonify({'msg': "등록완료"})
 
 
+@app.route("/board/post/reply", methods=["POST"])
+def board_post_reply():
+    reply_name_receive = request.form['reply_name_give']
+    reply_text_receive = request.form['reply_text_give']
+    reply_index_receive = request.form['reply_index_give']
+
+    insertDoc = {
+        'index': int(reply_index_receive),
+        'name': reply_name_receive,
+        'text': reply_text_receive
+    }
+
+    db.gameReview_reply.insert_one(insertDoc)
+
+    return jsonify({'msg': "등록완료"})
+
+
+@app.route("/board/get/reply", methods=["GET"])
+def board_get_reply():
+    board_reply_list = list(db.gameReview_reply.find({}, {'_id': False}))
+
+    return jsonify({'board_reply_list': board_reply_list})
+
+
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)

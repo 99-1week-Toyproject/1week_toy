09ae00fed5cc042f2782a81466eb1a1409d58adb
DESKTOP-M5B482U\이저우우 <wldnr1208@naver.com>
2022-12-10 12:06:01 +0900

    [#2][update]게시글에 리뷰 남기기 숨기기 기능 구현 필요

--- app.py
+++ app.py
@@ -1,9 +1,41 @@
-import requests
-from bs4 import BeautifulSoup
+from flask import Flask, render_template, request, jsonify
 
-headers = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
-data = requests.get('https://movie.naver.com/movie/sdb/rank/rmovie.naver?sel=pnt&date=20210829',headers=headers)
+app = Flask(__name__)
 
-soup = BeautifulSoup(data.text, 'html.parser')
+from pymongo import MongoClient
 
-# 코딩 시작
+client = MongoClient('mongodb+srv://test:sparta@cluster0.msaktnw.mongodb.net/cluster0?retryWrites=true&w=majority')
+db = client.dbsparta
+
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+
+@app.route("/game", methods=["POST"])
+def game_post():
+    game_receive = request.form['game_give']
+
+    game_list = list(db.game.find({}, {'_id': False}))
+    count = len(game_list) + 1
+
+    doc = {
+        'num': count,
+        'game': game_receive,
+        'done': 0
+    }
+
+    db.game.insert_one(doc)
+
+    return jsonify({'msg': '등록 완료!'})
+
+
+@app.route("/game", methods=["GET"])
+def game_get():
+    game_list = list(db.game.find({}, {'_id': False}))
+    return jsonify({'games': game_list})
+
+
+if __name__ == '__main__':
+    app.run('0.0.0.0', port=5002, debug=True)
